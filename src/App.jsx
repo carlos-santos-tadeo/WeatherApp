d485cbf6b71ae0a72c6397cedcb4f1f28dae9db2
carlos-santos-tadeo <cast0277@gmail.com>
2023-04-03 17:31:12 -0600

    responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -94,8 +94,8 @@ function App() {
         ) : (
           <div className=" grid place-content-center min-h-screen bg-[url(/images/bg1.jpg)] bg-cover px-2">
             <form onSubmit={handleSubmit} className='flex justify-center gap-2 py-4'>
-              <input id='textSearch' className='bg-slate-300/30 border-[1px] border-black rounded-md p-2' type="text" />
-              <button className='bg-red-500 p-2 text-white rounded-md'>Search</button>
+              <input id='textSearch' placeholder='Buscar el clima en otra ciudad' className='placeholder-black w-60 bg-slate-300/30 border-[1px] border-black rounded-md p-2 sm:w-[350px] sm:text-[22px] sm:placeholder-gray-600' type="text" />
+              <button className='bg-red-950 font-bold p-2 text-white rounded-md sm:text-[20px]'>Search</button>
 
             </form>
            { weather ? (
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -13,40 +13,40 @@ const Weather = ({ weather, temp }) => {
   return (
     <div>
       <section className='text-xl'>
-        <h2  className='bg-slate-300/50 rounded-3xl py-2 px-2 text-red-950	 text-center mb-4 font-bold text-[25px] tracking-wider sm:text-[35px]'>{weather.name}, {weather.sys.country}</h2>
+        <h2  className=' rounded-3xl py-2 px-2 text-red-1000 text-center mb-4 font-bold text-[25px] tracking-wider sm:text-[40px]'>{weather.name}, {weather.sys.country}</h2>
         {/* sm es media */}
-        <section className='grid gap-4 sm:grid-cols-two'>
-          <article className='bg-slate-300/50 rounded-3xl grid grid-cols-2 justify-items-center items-center py-2 sm:grid-cols-1'>
-            <h3 className='capitalize text-lg col-start-1 col-end-3 '>{weather.weather[0].description}</h3>
+        <section className='grid gap-4 sm:grid-cols-two justify-items-center'>
+          <article className='bg-slate-300/50 rounded-3xl grid grid-cols-2 justify-items-center items-center w-[280px] py-2 sm:grid-cols-1 sm:w-[350px] sm:py-1'>
+            <h3 className='capitalize text-lg col-start-1 col-end-3 sm:text-[25px]'>{weather.weather[0].description}</h3>
 
-            <h2 className='text-[45px] font-light sm:text-6xl'>{isCelsius ? `${temp.celsius}°C` : `${temp.farenheit}°F`}</h2>
+            <h2 className='text-[45px] font-light sm:text-7xl'>{isCelsius ? `${temp.celsius}°C` : `${temp.farenheit}°F`}</h2>
 
             <div>
-              <img src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
+              <img className='sm:w-[139px]' src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
             </div>
           </article>
 
           <article className='bg-slate-300/50 rounded-3xl grid grid-cols-3 justify-center justify-items-stretch py-2 sm:grid-cols-1 sm:px-2 sm:py-0'>
             <div className='flex text-base justify-center items-center'>
               <div><img src="/images/speed.png" alt="" /></div>
-              <h5>{weather.wind.speed} m/s</h5>
+              <h5 className='sm:text-[22px]'>{weather.wind.speed} m/s</h5>
             </div>
 
             <div className='flex text-base justify-center items-center'>
               <div><img src="/images/humidity.png" alt="" /></div>
-              <h5>{weather.main.humidity} %</h5>
+              <h5 className='sm:text-[22px]'>{weather.main.humidity} %</h5>
             </div>
 
             <div className='flex text-base justify-center items-center'>
               <div><img src="/images/pressure.png" alt="" /></div>
-              <h5>{weather.main.pressure} hPa</h5>
+              <h5 className='sm:text-[22px]'>{weather.main.pressure} hPa</h5>
             </div>
 
 
 
           </article>
         </section>
-        <button className='bg-blue-500 text-sm block mx-auto mt-4 py-2 px-6 text-white font-bold rounded-full hover:bg-blue-800 duration-200 sm:text-base' onClick={changeUnitTemp}>Change °C/°F</button>
+        <button className='bg-red-950 text-sm block mx-auto mt-4 py-2 px-6 text-white font-bold rounded-full hover:bg-blue-800 duration-200 sm:text-[25px] sm:py-4' onClick={changeUnitTemp}>Change °C/°F</button>
       </section>
     </div>
   )
