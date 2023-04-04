80848a3399b63c30630ccdf4032c486fca52d30f
carlos-santos-tadeo <cast0277@gmail.com>
2023-04-03 18:13:09 -0600

    add hover

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div className="grid place-content-center min-h-screen bg-cyan-400 bg-cover px-2">
+    <div className="grid place-content-center min-h-screen bg-red-900 bg-cover px-2">
       <div className='imgLoading'>
       </div>
       <h1 className='textLoading text-4xl text-white sm:text-7xl'>Weather App</h1>
