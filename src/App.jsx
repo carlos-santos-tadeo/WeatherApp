89d9197da4a97f7fded8ec46587d4ccadf9f5e3b
Carlos Santos <cast0277@gmail.com>
2023-08-16 22:40:13 -0600

    add-map

--- src/App.jsx
+++ src/App.jsx
@@ -1,56 +1,50 @@
-
-import axios from 'axios'
-import { useEffect, useState } from 'react'
-import Weather from './components/Weather'
-import Loader from './components/Loader'
-import Loading from './components/Loading'
-import './App.css'
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Weather from "./components/Weather";
+import Loader from "./components/Loader";
+import Loading from "./components/Loading";
+import "./App.css";
 
 function App() {
-
-  const [coords, setCoords] = useState()
-  const [weather, setWeather] = useState()
-  const [temp, setTemp] = useState()
-
+  const [coords, setCoords] = useState();
+  const [weather, setWeather] = useState();
+  const [temp, setTemp] = useState();
 
   //se ejecuta la funcion succes aqui y se declara una variable pos en la cual guardamos la latitud y longitud
   const success = (pos) => {
     const currentCoords = {
       lat: pos.coords.latitude,
-      lon: pos.coords.longitude
-    }
-    setCoords(currentCoords)
-  }
+      lon: pos.coords.longitude,
+    };
+    setCoords(currentCoords);
+  };
   //al estar vacio el [] se carga el useEffect y desde el primer render pide pregunta si aceptas la localizacion y ejecutamos la funcion success
   useEffect(() => {
-    navigator.geolocation.getCurrentPosition(success)
-
-  }, [])
-
+    navigator.geolocation.getCurrentPosition(success);
+  }, []);
 
   //en este efecto hacemos que dependa de coords lo pongo dentro de []
   useEffect(() => {
     if (coords) {
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=d8af59d241dcdbaa7036845048058d46`;
 
-      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=d8af59d241dcdbaa7036845048058d46`
-
-      axios.get(URL)
+      axios
+        .get(URL)
         .then((res) => {
-          setWeather(res.data)
-          const celsius = (res.data.main.temp - 273.15).toFixed(1)
-          const farenheit = (celsius * (9 / 5) + 32).toFixed(1)
+          setWeather(res.data);
+          const celsius = (res.data.main.temp - 273.15).toFixed(1);
+          const farenheit = (celsius * (9 / 5) + 32).toFixed(1);
           const newTemps = {
             celsius,
-            farenheit
-          }
-          setTemp(newTemps)
-         
+            farenheit,
+          };
+          setTemp(newTemps);
         })
-        .catch((err) => console.log(err))
+        .catch((err) => console.log(err));
     }
-  }, [coords])
+  }, [coords]);
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -60,58 +54,51 @@ function App() {
   }, []);
 
   const handleSubmit = (e) => {
-    e.preventDefault()
-    const query = e.target.textSearch.value
-    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=d8af59d241dcdbaa7036845048058d46`
-
+    e.preventDefault();
+    const query = e.target.textSearch.value;
+    const URL = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=d8af59d241dcdbaa7036845048058d46`;
 
     if (coords) {
-
-
-      axios.get(URL)
+      axios
+        .get(URL)
         .then((res) => {
-          setWeather(res.data)
-          const celsius = (res.data.main.temp - 273.15).toFixed(1)
-          const farenheit = (celsius * (9 / 5) + 32).toFixed(1)
+          setWeather(res.data);
+          const celsius = (res.data.main.temp - 273.15).toFixed(1);
+          const farenheit = (celsius * (9 / 5) + 32).toFixed(1);
           const newTemps = {
             celsius,
-            farenheit
-          }
-          setTemp(newTemps)
+            farenheit,
+          };
+          setTemp(newTemps);
         })
-        .catch((err) => console.log(err))
+        .catch((err) => console.log(err));
     }
-
-  }
-
+  };
 
   return (
-    <div >
-
-      {
-        loading ? (
-          <Loading />
-        ) : (
-          <div className=" grid place-content-center min-h-screen bg-[url(/images/bg.jpg)] bg-cover px-2">
-            <form onSubmit={handleSubmit} className='flex justify-center gap-2 py-4'>
-              <input id='textSearch' placeholder='Search in another city' className='placeholder-black w-60 bg-slate-300/30 border-[1px] border-black rounded-md p-2 sm:w-[350px] sm:text-[22px] sm:placeholder-gray-600' type="text" />
-              <button className='bg-red-900 hover:bg-blue-800 font-bold p-2 text-white rounded-md sm:text-[20px]'>Search</button>
-
-            </form>
-           { weather ? (
-            <Weather weather={weather} temp={temp} />
-            ) : (
-            <Loader />
-            )
-}
-          </div>
-
-        )
-      }
-
+    <div>
+      {loading ? (
+        <Loading />
+      ) : (
+        <div className=" grid place-content-center min-h-screen bg-[url(/images/bg.jpg)] bg-cover px-2">
+          <form
+            onSubmit={handleSubmit}
+            className="flex justify-center gap-2 py-4"
+          >
+            <input
+              id="textSearch"
+              placeholder="Search in another city"
+              className="placeholder-black w-60 bg-slate-300/30 border-[1px] border-black rounded-md p-2 sm:w-[350px] sm:text-[22px] sm:placeholder-gray-600"
+              type="text"
+            />
+            <button className="bg-red-900 hover:bg-blue-800 font-bold p-2 text-white rounded-md sm:text-[20px]">
+              Search
+            </button>
+          </form>
+          {weather ? <Weather weather={weather} temp={temp} /> : <Loader />}
+        </div>
+      )}
     </div>
-
-
-  )
+  );
 }
-export default App
+export default App;
--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,55 +1,78 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const Weather = ({ weather, temp }) => {
   // console.log(weather)
-  const [isCelsius, setIsCelsius] = useState(true)
+  const [isCelsius, setIsCelsius] = useState(true);
 
   const changeUnitTemp = () => {
-    setIsCelsius(!isCelsius)
-  }
-
-
+    setIsCelsius(!isCelsius);
+  };
 
   return (
     <div>
-      <section className='text-xl'>
-        <h2  className=' rounded-3xl py-2 px-2 text-red-1000 text-center mb-4 font-bold text-[25px] tracking-wider sm:text-[40px]'>{weather.name}, {weather.sys.country}</h2>
+      <section className="text-xl">
+        <h2 className=" rounded-3xl py-2 px-2 text-red-1000 text-center mb-4 font-bold text-[25px] tracking-wider sm:text-[40px]">
+          {weather.name}, {weather.sys.country}
+        </h2>
         {/* sm es media */}
-        <section className='grid gap-4 sm:grid-cols-two justify-items-center'>
-          <article className='bg-slate-300/50 rounded-3xl grid grid-cols-2 justify-items-center items-center w-[280px] py-2 sm:grid-cols-1 sm:w-[350px] sm:py-1'>
-            <h3 className='capitalize text-lg col-start-1 col-end-3 sm:text-[25px]'>{weather.weather[0].description}</h3>
+        <section className="grid gap-4 sm:grid-cols-two justify-items-center">
+          <article className="bg-slate-300/50 rounded-3xl grid grid-cols-2 justify-items-center items-center w-[280px] py-2 sm:grid-cols-1 sm:w-[350px] sm:py-1">
+            <h3 className="capitalize text-lg col-start-1 col-end-3 sm:text-[25px]">
+              {weather.weather[0].description}
+            </h3>
 
-            <h2 className='text-[45px] font-light sm:text-7xl'>{isCelsius ? `${temp.celsius}°C` : `${temp.farenheit}°F`}</h2>
+            <h2 className="text-[45px] font-light sm:text-7xl">
+              {isCelsius ? `${temp.celsius}°C` : `${temp.farenheit}°F`}
+            </h2>
 
             <div>
-              <img className='sm:w-[139px]' src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt="" />
+              <img
+                className="sm:w-[139px]"
+                src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt=""
+              />
             </div>
           </article>
 
-          <article className='bg-slate-300/50 rounded-3xl grid grid-cols-3 justify-center justify-items-stretch py-2 sm:grid-cols-1 sm:px-2 sm:py-0'>
-            <div className='flex text-base justify-center items-center'>
-              <div><img src="/images/speed.png" alt="" /></div>
-              <h5 className='sm:text-[22px]'>{weather.wind.speed} m/s</h5>
+          <article className="bg-slate-300/50 rounded-3xl grid grid-cols-3 justify-center justify-items-stretch py-2 sm:grid-cols-1 sm:px-2 sm:py-0">
+            <div className="flex text-base justify-center items-center">
+              <div>
+                <img src="/images/speed.png" alt="" />
+              </div>
+              <h5 className="sm:text-[22px]">{weather.wind.speed} m/s</h5>
             </div>
 
-            <div className='flex text-base justify-center items-center'>
-              <div><img src="/images/humidity.png" alt="" /></div>
-              <h5 className='sm:text-[22px]'>{weather.main.humidity} %</h5>
+            <div className="flex text-base justify-center items-center">
+              <div>
+                <img src="/images/humidity.png" alt="" />
+              </div>
+              <h5 className="sm:text-[22px]">{weather.main.humidity} %</h5>
             </div>
 
-            <div className='flex text-base justify-center items-center'>
-              <div><img src="/images/pressure.png" alt="" /></div>
-              <h5 className='sm:text-[22px]'>{weather.main.pressure} hPa</h5>
+            <div className="flex text-base justify-center items-center">
+              <div>
+                <img src="/images/pressure.png" alt="" />
+              </div>
+              <h5 className="sm:text-[22px]">{weather.main.pressure} hPa</h5>
             </div>
-
-
-
           </article>
         </section>
-        <button className='bg-red-950 text-sm block mx-auto mt-4 py-2 px-6 text-white font-bold rounded-full hover:bg-blue-800 duration-200 sm:text-[25px] sm:py-4' onClick={changeUnitTemp}>Change °C/°F</button>
+        <button
+          className="bg-red-950 text-sm block mx-auto mt-4 py-2 px-6 text-white font-bold rounded-full hover:bg-blue-800 duration-200 sm:text-[25px] sm:py-4"
+          onClick={changeUnitTemp}
+        >
+          Change °C/°F
+        </button>
+        <div className="mt-5 overflow-hidden pb-[56.25%] relative h-0">
+          <iframe
+            className=" left-0 top-0 h-[100%] w-[100%] absolute"
+            src={`https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d1898056.043331925!2d${weather.coord.lon}!3d${weather.coord.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses-419!2smx!4v1692246731467!5m2!1ses-419!2smx`}
+            style={{ border: 0 }}
+          ></iframe>
+        </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export default Weather
+export default Weather;
